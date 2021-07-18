a5a65de2df80e8a0196271394ee24ac22daedfee
investment4057 <investment4057@gmail.com>
2021-07-19 00:20:00 +0900

    セッションのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
-  resources :tasks
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   get 'signup', to: 'users#new'
+  resources :tasks
   resources :users, only: [:create]
 end
